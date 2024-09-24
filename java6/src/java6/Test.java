5db0d348888ad236043e87fdecac66f87db724bd
ramnath98 <dongareramnath55@gmail.com>
2024-09-24 12:02:05 -0400

    update method m1  by Ramnath

--- java6/src/java6/Test.java
+++ java6/src/java6/Test.java
@@ -8,35 +8,7 @@ public class Test {
 		
 	}
 	
-	public void m2()
-	{
-		System.out.println("m2 ");	
-		
-	}
-
-	public void m3()
-	{
-		System.out.println("m3");	
-		
-	}
 	
-	public void m4()
-	{
-		System.out.println("m4");	
-		
-	}
-	
-	public void m5()
-	{
-		System.out.println("m5");	
-		
-	}
-	
-	public void m6()
-	{
-		System.out.println("m6");	
-		
-	}
 
 
 }
